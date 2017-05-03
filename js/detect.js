d8cb083db8651f5b1de79e239c1847e379275490
caron <etienne.caron@inist.fr>
2017-05-03 17:13:10 +0200

    WIP : Détection de Chrome

--- js/detect.js
+++ js/detect.js
@@ -1,7 +1,24 @@
 "use strict";
 
 if (bowser.chrome || bowser.chromium) {
-  console.log('Chrome');
+  $('#addingToBrowser').text('Ajouter l\'extension à Chrome');
+  if (chrome.app.isInstalled) {
+    //console.log('Installé');
+    $('#addingToBrowser').text('Extension déjà ajoutée !');
+    $('#addingToBrowser').prop("disabled", true);
+  } else {
+    //console.log('Event');
+    $('#addingToBrowser').click(function() {
+      //console.log('Click');
+      chrome.webstore.install(undefined, function() {
+        //console.log('Success');
+        $('#addingToBrowser').text('Extension bien ajoutée !');
+        $('#addingToBrowser').prop("disabled", true);
+      }, function(msg){
+        console.log('Error :' + msg);
+      });
+    });
+  };
 } else if (bowser.firefox) {
   console.log('Firefox');
   if (bowser.check({ firefox: "49" })) {
